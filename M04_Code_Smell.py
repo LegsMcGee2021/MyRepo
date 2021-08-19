cbb9be45dd911f4d5c73472af73a2ca65f1ca089
LegsMcGee2021 <ctreybailey@gmail.com>
2021-08-19 15:28:54 -0400

    Adding MO4_Code_Smell and MO5_Code_Smell

---
+++ M04_Code_Smell.py
@@ -0,0 +1,14 @@
+def is_num():
+    while True:
+        try:
+            x = int(input("Show me what you got.\n"))
+        except ValueError:
+            print("Disqualified!")
+        else:
+            return x
+
+
+# From here down should not be changed
+
+num = is_num()
+print(f"You got {int(num)}.")
